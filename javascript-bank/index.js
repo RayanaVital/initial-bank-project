618de33b046eb49b717b592c67a99c61daaf74cb
Rayana Vital <rayanavital@gmail.com>
2022-05-20 11:24:02 -0300

    Commit inicial com funcionalidades de saque e deposito.

---
+++ javascript-bank/index.js
@@ -0,0 +1,41 @@
+class Cliente {
+    nome;
+    cpf;
+    
+}
+
+
+class ContaCorrente {
+    agencia;
+    _saldo = 0;
+
+    sacar(valor){
+
+        if (this._saldo >= valor) {
+
+            this._saldo -= valor;
+        }
+    }
+
+    depositar(valor){
+        if (valor > 0){
+
+            this._saldo += valor;
+        }
+    }
+}
+
+const cliente1 = new Cliente();
+cliente1.nome = "Ricardo";
+cliente1.cpf = 11122233309;
+
+const cliente2 = new Cliente();
+cliente2.nome = "Alice";
+cliente2.cpf = 22233309;
+
+const ContaCorrenteRicardo = new ContaCorrente;
+ContaCorrenteRicardo.agencia = 1001;
+ContaCorrenteRicardo.depositar(100);
+ContaCorrenteRicardo.sacar(50);
+console.log(ContaCorrenteRicardo._saldo);
+
