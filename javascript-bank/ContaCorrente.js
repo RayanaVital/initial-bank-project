b3494ec90444ef1b961175f5c5ce2053bfe13145
Rayana Vital <rayanavital@gmail.com>
2022-05-23 15:18:44 -0300

    Adicionando getters and setters.

--- javascript-bank/ContaCorrente.js
+++ javascript-bank/ContaCorrente.js
@@ -1,6 +1,25 @@
-class ContaCorrente {
+import { Cliente } from "./Cliente.js";
+
+export class ContaCorrente {
     agencia;
+    _cliente;
     _saldo = 0;
+
+    set cliente(novoValor){
+        if(novoValor instanceof Cliente){
+            this._cliente = novoValor;
+        }
+        
+    }
+
+    get cliente(){
+        return this.cliente;
+    }
+
+
+    get saldo(){
+        return this._saldo;
+    }
 
     sacar(valor) {
 
@@ -17,5 +36,10 @@ class ContaCorrente {
         this._saldo += valor;
         return valor;
 
+    }
+
+    transferir(valor, conta){
+        const valorSacado = this.sacar(valor);
+        conta.depositar(valorSacado);
     }
 }
